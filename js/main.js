7cab2adb58737b332c7bb22beb34f0a46aeaabed
ZahidulHoque09 <zahin_m96@yahoo.com>
2024-01-29 06:16:54 +0100

    Making of namta application

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/**Making of Namta apllication */
+
+document.write("Example to make loop in Java script: <br/> <br/>");
+
+let num = prompt("Enter number: ");
+let namta = +num;
+for (let i = 1; i <= 10; i++) {
+  /*  console.log(namta + "x" + i + "=" + namta * i); */
+  /*  document.write(namta + "x" + i + "=" + namta * i + "<br/>"); */
+  /* 
+  console.log(`${namta} x ${i} = ${namta * i}`); */
+
+  document.write(`${namta} x ${i} = ${namta * i}  <br/>`);
+}
